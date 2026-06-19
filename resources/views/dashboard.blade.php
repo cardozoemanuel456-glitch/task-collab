bf7ff2220f49edd3ab5a5685eca0d3ac4b05032b
Cardozo-Benjamin <cardozoemanuel456@gmail.com>
2026-06-19 11:00:05 -0300

    pruebas de calidad en el dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-<<<<<<< HEAD
-=======
+
             
->>>>>>> dvlp/GinesFabrizio
+
             <livewire:task-board />
         </div>
     </div>
